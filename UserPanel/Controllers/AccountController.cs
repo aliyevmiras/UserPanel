fedc74c17aa8535b516767e67ee6ce3772d53d96
Miras Aliyev <aliyevmiras@gmail.com>
2024-05-04 01:36:34 +0500

    feat: add login view with form to send post http request to action method which currently do nothing

---
+++ UserPanel/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UserPanel.Models;
+
+namespace UserPanel.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public AccountController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            Debug.WriteLine($"User email: {user.Email}");
+            Debug.WriteLine($"User password: {user.Password}");
+            return View();
+        }
+
+    }
+}
