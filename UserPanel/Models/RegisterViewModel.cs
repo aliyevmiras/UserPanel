c17854d3b42adbd259df2eb931fc189650fb0cea
Miras Aliyev <aliyevmiras@gmail.com>
2024-07-01 22:11:11 +0500

    feat: add RegisterViewModel with validation rules

---
+++ UserPanel/Models/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserPanel.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public required string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Please provide a valid email address")]
+        [DataType(DataType.EmailAddress)]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Please provide a valid password")]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Please provide a valid password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match")]
+        [DataType(DataType.Password)]
+        public required string ConfirmPassword { get; set; }
+    }
+}
