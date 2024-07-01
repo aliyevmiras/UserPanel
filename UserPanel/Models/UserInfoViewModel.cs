af3ec1b1e29de9cd45472c92ab1f5ea4cbfe4297
Miras Aliyev <aliyevmiras@gmail.com>
2024-07-01 22:17:47 +0500

    feat: add UserInfoViewModel to expose only required data to the view (validation is not required)

---
+++ UserPanel/Models/UserInfoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UserPanel.Models
+{
+    public class UserInfoViewModel
+    {
+        public required string Email { get; set; }
+
+        public required string UserName { get; set; }
+
+        public required DateTime RegistrationDate { get; set; }
+    }
+}
