8cb3892a8c6111b21608d3abd1d2863676131729
Miras Aliyev <aliyevmiras@gmail.com>
2024-07-01 22:10:04 +0500

    refactor: change LoginViewModel validation rules using data annotations

--- UserPanel/Models/LoginViewModel.cs
+++ UserPanel/Models/LoginViewModel.cs
@@ -4,15 +4,13 @@ namespace UserPanel.Models
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "Please provide a valid email address")]
+        [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Please provide a valid email address")]
         [DataType(DataType.EmailAddress)]
-        [Display(Prompt = "johndoe@xxxxx.xxx")]
         public required string Email { get; set; }
 
-        [Required(ErrorMessage = "Please provide a valid password")]
+        [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
-        [Display(Prompt = "Must have at least 1 character")]
         public required string Password { get; set; }
     }
 }
