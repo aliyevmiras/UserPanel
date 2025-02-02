7c5c31e890c52d700cd6324af2c629935cfd0047
Miras Aliyev <aliyevmiras@gmail.com>
2025-02-02 20:42:45 +0500

    fix: add proper behaviour of checkbox "Select All" button

--- UserPanel/wwwroot/js/site.js
+++ UserPanel/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.getElementById('selectAllCheckbox')
+    .addEventListener('change', function () {
+        let checkboxes =
+            document.querySelectorAll('.checkbox');
+        checkboxes.forEach(function (checkbox) {
+            checkbox.checked = this.checked;
+        }, this);
+    });
